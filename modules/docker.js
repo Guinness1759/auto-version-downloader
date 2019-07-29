8906a8db62e14f3d07fc01b3cf30d6e2bcddb0ea
user5 <user5@devel-ws.devstations.local>
2019-07-29 17:53:50 +0300

    Patched to support docker API change

--- modules/docker.js
+++ modules/docker.js
@@ -3,14 +3,20 @@ const request = require('request-promise-native');
 
 const VERSION_PAGE_URL = 'https://download.docker.com/linux/static/stable/x86_64/';
 
+function basename(p) {
+    return path.basename(p, path.extname(p));
+}
+
 // Uses HTML regex manipulation. This is bad but for now there is no other way
 module.exports = async (logger, netOptions) => {
     const version_page = await request(VERSION_PAGE_URL, { netOptions });
 	const versions = version_page.match(/href="([\w\d-.]*)"/g);
-    const latest = versions[versions.length - 1].split('"')[1]
+    const latest_parts = versions[versions.length - 1].split('"')[1].split('-');
+    const latest = basename(latest_parts[latest_parts.length - 1]);
     return {
-        [path.basename(latest.split('-')[1], path.extname(latest))]: [
-			`${VERSION_PAGE_URL}${latest}`,
+        [latest]: [
+            `${VERSION_PAGE_URL}docker-${latest}.tgz`,
+            `${VERSION_PAGE_URL}docker-rootless-extras-${latest}.tgz`,
         ],
-    }
+    };
 }
